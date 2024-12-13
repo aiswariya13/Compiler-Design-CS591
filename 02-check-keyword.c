d4c210d4bc31e4f2bfce4c17551e1ec06d886e79
Tijo <hamsen3880@outlook.com>
2024-12-13 07:46:08 +0530

    upload

--- 02-check-keyword.c
+++ 02-check-keyword.c
@@ -7,9 +7,10 @@ void main(void) {
     int flag = 0, i;
     char str[80];
     char* words[] = {"auto", "break", "case", "int", "float", "char", "if", "void", "sizeof"};
+    int num = sizeof(words) / sizeof(words[0]);
     printf("Enter a lexeme: ");
     scanf("%s", str);
-    for (i = 0; i < 31; i++) {
+    for (i = 0; i < num; i++) {
         if (strcmp(str, words[i]) == 0) {
             flag = 1;
             break;
